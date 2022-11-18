ad4b44fb23af0f4ac900ea07e3b7eb04d6a9ee88
Novak <cnovak@github.com>
2022-11-18 18:39:24 -0500

    add variable noise to signals and OLA structure for ANC algs

--- ASP_Project_Driver.m
+++ ASP_Project_Driver.m
@@ -7,7 +7,7 @@ audiodir = './ASP_Project_Audio/';
 listname = dir(audiodir);
 listname = listname(3:end);
 fs = 44100;
-t_per_song = 10; % 20 second clips of each song
+t_per_song = 5; % 5 second clips of each song
 num_samples = t_per_song * fs;
 music_files = {};
 for i = 1:length(listname)
@@ -16,26 +16,49 @@ for i = 1:length(listname)
 end
 
 
-%% Add Noise
+%% Add variable noise noise
+x = music_files{1};
 
-x = music_files{i};
 % make mono for now
 x = mean(x,2);
-xlen = length(x);
-gain = .01;
-mean = 0;
-noise = gain * randn(xlen,1) + mean;
-figure;
 
+xlen = length(x);
+noiselen = 1 * fs; % change noise every 1 sec
+num_frames = xlen / noiselen;
+xn = zeros(xlen,1); % signal + noise
+sn = zeros(xlen,1); % noise
+gains = [.01,.05,.01,.08,.005]; % can change this 
+
+for f = 0:num_frames-1
+    ind = 1+f*noiselen:noiselen+f*noiselen;
+    noise = gains(f+1)*randn(noiselen,1);
+    xn(ind) = x(ind) + noise;
+    sn(ind) = noise;
+end
+
+figure;
 subplot(311);
 plot(x);
-xnoise = x + noise;
 subplot(312);
-plot(noise); 
+plot(sn); 
 subplot(313);
-plot(xnoise);
+plot(xn);
 
-%% Pre process WOLA
+%% Run adaptive noise cancelation process
+
+win_len = 512; % define in samples for now
+
+% hann window - 50% overlap negates need for normalization
+win = hann(win_len,"periodic")';
+hop = win_len / 2; 
+
+ys = run_anc(xn,sn,win,hop);
+
+%% Compare SNR
+
+
+
+
 
 
 
